3b76127c87559d2b26f0b612c1a2da82f0a2b59d
Mathias <65356596+mathiaswdr@users.noreply.github.com>
2026-01-12 17:29:27 +0100

    updates 2026-01-12 17:29:26

--- app/(site)/auth/login/page.tsx
+++ app/(site)/auth/login/page.tsx
@@ -4,5 +4,9 @@ import { LoginForm } from "@/components/auth/login-form";
 export default function Login(){
 
 
-    return <LoginForm />
+    return (
+        <section className="min-h-screen w-full flex items-center justify-center px-4 py-10">
+            <LoginForm />
+        </section>
+    );
 }
